dc8ce3fce38763f183ccaabcdfd767f636ab0f58
Benclark00 <78094988+Benclark00@users.noreply.github.com>
2023-09-07 15:13:24 -0400

    second commit to this file

--- Python4DS/Concatenation&Transformation.py
+++ Python4DS/Concatenation&Transformation.py
@@ -20,3 +20,23 @@ print(pd.concat([df_obj, df_obj2]))
 
 """^^^^These leave some spots as NaN as the tables were not of equal size"""
 
+'''Transforming Data'''
+"""Drops unwanted data from rows indexed at 0 and 2"""
+print(df_obj.drop([0,2]))
+
+'''Can change this to columns with the addition of axis = 1 after the closing ] '''
+
+"""Adding Data"""
+'''Creates new series to be added to the DF'''
+s_obj = Series(np.arange(6))
+s_obj.name = "added_variable"
+print(s_obj)
+
+'''Joining the 2 obj'''
+"""Join Method"""
+variable_added= DataFrame.join(df_obj, s_obj)
+print(variable_added)
+
+"""Append Method"""
+added_datatable = variable_added.append(variable_added, ignore_index = False)
+print(added_datatable)
